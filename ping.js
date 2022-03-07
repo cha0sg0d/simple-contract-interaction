869a3875f6a933b5a32e2e725c4a480ecdae7210
cha0sg0d <tony.goss@protonmail.com>
2022-03-06 19:37:06 -0800

    feat: ethers js example

---
+++ ping.js
@@ -0,0 +1,27 @@
+import {ethers} from 'ethers';
+import { CONTRACT_ADDRESS } from '@darkforest_eth/contracts';
+import ABI from './DarkForest.js'
+
+const PLAYER_ADDRESS = '0x26e695d87d68a738736059201d362501abbc4e1d';
+const XDAI = 'https://rpc.xdaichain.com/'
+
+const provider = new ethers.providers.JsonRpcProvider(XDAI);
+
+
+
+const main = async () => {
+    const core = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
+    try {
+        const constants = await core.getGameConstants();
+        console.log('constants', JSON.stringify(constants));
+        
+    } catch (error) {
+        console.log("error getting bulk", error);
+    }
+
+}
+
+main().catch(console.log);
+
+
+
